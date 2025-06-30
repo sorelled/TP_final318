53e3d25699961365443210791841f82235bd01fe
sorelleD <sorelled039@gmail.com>
2025-06-30 17:52:45 +0100

    featurs/2-model

--- src/main/java/com/hinkaku/product_manager/model/Category.java
+++ src/main/java/com/hinkaku/product_manager/model/Category.java
@@ -6,6 +6,7 @@ import lombok.*;
 import java.util.List;
 import java.util.UUID;
 
+
 @Entity
 @Table(name = "categories")
 @Data
--- src/main/java/com/hinkaku/product_manager/model/Departement.java
+++ src/main/java/com/hinkaku/product_manager/model/Departement.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import java.util.UUID;
 
+
 @Entity
 @Table(name = "departements")
 @Data
